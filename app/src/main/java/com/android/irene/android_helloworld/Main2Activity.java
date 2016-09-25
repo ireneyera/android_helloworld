a7e42ce3fcedb8a3fe750f4f1634506e90b33c2f
ireneyera <irene.yera@gmail.com>
2016-09-25 11:00:14 +0200

    Version 2

--- app/src/main/java/com/android/irene/android_helloworld/Main2Activity.java
+++ app/src/main/java/com/android/irene/android_helloworld/Main2Activity.java
@@ -4,13 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     private static final String TAG = "Hello world";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         Log.i(TAG, "onCreate");
     }
 
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "onStart");
+        Log.i(TAG, "onResume");
     }
     @Override
     protected void onPause() {
